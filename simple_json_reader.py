6afb9163e13355c6fe154130c6ebfb428cb770d7
Gal Anonim <metntalisa@gmail.com>
2024-04-10 07:12:49 +0200

    Docstrings
    I have prepared docstrings for functions, methods and classes in Python files.

--- simple_json_reader.py
+++ simple_json_reader.py
@@ -2,6 +2,15 @@ import json
 from rich import print
 
 def verify_json(file_path):
+    """
+    Verify JSON file for specific conditions.
+
+    Parameters:
+    file_path (str): The path to the JSON file.
+
+    Returns:
+    bool: Return False if the key:'Resource' have value:'*', True otherwise.
+    """
     with open(file_path, 'r') as f:
         data = json.load(f)
         if 'Statement' in data['PolicyDocument']:
